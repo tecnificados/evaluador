aa0216d57f111c9df456d9115c67a7825b514fcc
Juan Carlos Ballesteros Hermida <jcballesteroshermida@gmail.com>
2020-03-01 16:42:02 +0100

    Comandos git añadidos para la ejecución en Termux

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+date=$(date '+%Y-%m-%d %H:%M:%S')
+echo $date
+git pull
 rm -f datosgobes.csv
 wget http://ondemand2.redes.ondemand.flumotion.com/redes/ondemand2/Datosabiertos/datosgobes.csv
 java -jar evaluador.jar
+git add .
+git commit -m "Informes actualizados $date"
+git push
+
